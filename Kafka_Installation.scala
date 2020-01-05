0a9ee116ef069eeb28665bf31f0cce57c2224db2
Sankar Biswas <sankarb475@gmail.com>
2020-01-05 08:59:51 +0530

    Create Kafka_Installation.scala

---
+++ Kafka_Installation.scala
@@ -0,0 +1,30 @@
+Zookeeper:
+================================
+Kafka uses Zookeeper to store the metadata about kafka cluster. Broker and kafka topic details get stored in Zookeeper as well as the
+consumer client details such as offset values. You can use zookeeper script placed in kafka distribution, but full version of zookeeper
+is not really required for Kafka.
+
+parameters to be set :
+> tickTime=2000
+> dataDir=/var/lib/zookeeper
+> clientPort=2181
+
+> zkServer.sh start  
+-- this is how you start your zookeeper server.
+
+** A zookeeper server is called ensemble, and you should have odd numbers of nodes in an ensemble.
+
+Kafka Broker
+===============================
+Start the kafka broker:
+> kafka-server-start.sh -daemon /usr/local/kafka/config/server.properties
+
+Creating and verifying a topic::
+> kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic test
+
+Verifying the existing topic::
+> kafka-topics.sh --zookeeper localhost:2181 --describe --topic test 
+
+-- for standalone kafka, this will suffice. But for a Kafka cluster to be set up, we would need to set many config parameters.
+
+*** broker.id ==> Each broker should have unique value for this within a single kafka cluster. By default it is set to 0. 
