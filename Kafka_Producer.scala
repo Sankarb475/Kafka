4f9eb4c45606d33debbcd4c43b99721d9cedfb4b
Sankar Biswas <sankarb475@gmail.com>
2020-01-07 01:30:25 +0530

    Update Kafka_Producer.scala

--- Kafka_Producer.scala
+++ Kafka_Producer.scala
@@ -70,11 +70,65 @@ We send a message, the send() method returns a Future object, and we use get() t
 or not.
 
 
+try{
+    producer.send(record).get()
+}
+catch{
+    case IOException => {
+        print("Error")
+    }
+}
+
+this method will throw an error if the message is not successfully sent to Kafka. We can extract the last offset value.
+
+3) Asynchronous send ==>
+We call the send() method with a callback function, which gets triggered when it receives a response from the Kafka broker.
 
 
+Producer config parameters
+================================================================
+client.id
+==> This can be any string, and will be used by the brokers to identify messages sent from the client. It is used in logging and 
+metrics, and for quotas.
+
+max.request.size
+==> by default this is 1 MB.
+Maximum size of each record sent over by the Producer to the broker without a failure. This also hints the number of messages you can
+send over each batch. If you have records with 1 KB, you can send 1000 records each batch.
+
+acks ==> <integer number> 
+acks = all, all in-sync replicas has to be updated and only after that Kafka will send success message.
+this represents minimum number of replicas which has to be have the message written to before Kafka calls the message 
+writing has been successful.
+
+compression.type
+==> by default messages are sent uncompressed. We can compress the messages - which will lead to better performance over a use of
+processing power. This parameter can be set to snappy, gzip, or lz4.
+
+retries
+==> number of retries Producer will do before sending an error message to the client.
+
+batch.size
+==> The thresold size of each batch of messages, when the combined size of all the messages crosses this value producer sends one batch.
+
+*** If either your key or value is not an integer or a string you can use Avro Serializer to serialize.
+> props.put("key.serializer", "io.confluent.kafka.serializers.KafkaAvroSerializer");
 
 
+Kafka keys
+========================================================================
+Keys serve two goals :
+1) Additional information that gets stored with the message
+2) In which partition one record will be written to. 
 
+All messages with the same key will go to the same partition. This means that if a process is reading only a subset of the partitions 
+in a topic (more on that in Chapter 4), all the records for a single key will be read by the same process.
 
+new ProducerRecord[String,String](topic, key, value)
+
+If a key exists and the default partitioner is used, Kafka will hash the key (using its own hash algorithm, so hash values will not 
+change when Java is upgraded), and use the result to map the message to a specific partition. 
+When the key is null and the default partitioner is used, the record will be sent to one of the available partitions of the topic at 
+random.
 
 
