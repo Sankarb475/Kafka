8b00c56b6344fdd3b7d3bd27c0072b79cb6e7176
Sankar Biswas <sankarb475@gmail.com>
2020-01-06 19:44:10 +0530

    Update Kafka_Installation.scala

--- Kafka_Installation.scala
+++ Kafka_Installation.scala
@@ -18,6 +18,15 @@ zookeeper-server-start.sh config/zookeeper.properties
 
 Kafka Broker
 ===============================
+The nodes which run as kafka brokers has to have these config parameters updated:
+
+broker.id = <unique integer within a cluster>
+zookeeper.conect = <IP_Address1>:<port = 2181>,<IP_Adress2>:<port = 2181>,.......
+
+Optional ==> zookeeper.connection.timeout.ms=6000
+# A comma seperated list of directories under which to store log files
+log.dirs=/tmp/kafka-logs
+
 Start the kafka broker:
 > kafka-server-start.sh -daemon /usr/local/kafka/config/server.properties
 
@@ -30,6 +39,26 @@ for newer version of kafka
 Verifying the existing topic::
 > kafka-topics.sh --zookeeper localhost:2181 --describe --topic test 
 
+
+log.dirs ==> 
+Kafka persists all messages to disk, and these log segments are stored in the directories specified in the log.dirs configuration. 
+This is a comma-separated list of paths on the local system. If more than one path is specified, the broker will store partitions on
+them in a “least-used” fashion with one partition’s log segments stored within the same path. Note that the broker will place a new 
+partition in the path that has the least number of partitions currently stored in it, not the least amount of disk space
+used in the following situations
+
+log.retention.ms ==>
+The message retention time for kafka, by default it is 168 hours that is 1 week.
+We can configure it using this parameter.
+
+log.retention.bytes ==>
+This is another way to delete the records. So, this parameter defines the maximum size of a partition in Kafka topic.
+If you have 8 partitions on a topic, and this parameter is set to 1 GB, your partition at max will be able to store 8 GB of data.
+
+message.max.bytes ==>
+This sets the maximum record size a producer can write to a kafka partition. By default it is 1 MB.
+
+
 Listing out all the topics created ::
 ======================================
 > bin/kafka-topics.sh --list --bootstrap-server localhost:9092
@@ -41,5 +70,12 @@ Listing out all the topics created ::
 *** num.partitions ==> The num.partitions parameter determines how many partitions a new topic is created with, primarily when automatic 
 topic creation is enabled (which is the default setting). This parameter defaults to one partition. Keep in mind that the number of
 partitions for a topic can only be increased, never decreased.
+
+
+Kafka Consumer and Producer set up::
+==========================================
+metadata.broker.list=PUBLIC_IP_ADDRESS_OF_FIRST_KAFKA_BROKER:9092, PUBLIC_IP_ADDRESS_OF_SECOND_KAFKA_BROKER:9092
+
+
 
 
