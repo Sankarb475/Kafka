63b9113c983dd82477ecc843bbd79502e11cad87
Sankar Biswas <sankarb475@gmail.com>
2020-01-07 15:36:00 +0530

    Create Kafka_Consumer.scala

---
+++ Kafka_Consumer.scala
@@ -0,0 +1,7 @@
+Kafka Consumer
+=====================================================================================================================================
+Applications that need to read data from Kafka use a KafkaConsumer to subscribe to Kafka topics and receive messages from these topics.
+
+
+Consumers and Consumer Groups
+===============================================================
